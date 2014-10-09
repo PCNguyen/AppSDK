c3abcbc453fb58c95afafae46d64d6a051be91dd
PC Nguyen <pnguyen@reputation.com>
2014-10-09 16:19:26 -0700

    test migration to header file

--- AppLib/AppLib.h
+++ AppLib/AppLib.h
@@ -2,14 +2,15 @@
 //  AppLib.h
 //  AppSDK
 //
-//  Created by PC Nguyen on 5/15/14.
+//  Created by PC Nguyen on 10/9/14.
 //  Copyright (c) 2014 PC Nguyen. All rights reserved.
 //
+
+#ifndef AppSDK_AppLib_h
+#define AppSDK_AppLib_h
 
 #import "AppLibExtension.h"
 #import "AppLibValueTransformer.h"
 #import "AppLibScheduler.h"
 
-@interface AppLib : NSObject
-
-@end
+#endif
