3d6497c0b029ae7c7bbfa1c9b2b9b7e2c0f8f798
PC Nguyen <pnguyen@reputation.com>
2014-10-10 11:26:49 -0700

    adding AppLibShared header

---
+++ AppLib/Shared/AppLibShared.h
@@ -0,0 +1,13 @@
+//
+//  AppLibShared.h
+//  AppSDK
+//
+//  Created by PC Nguyen on 10/10/14.
+//  Copyright (c) 2014 PC Nguyen. All rights reserved.
+//
+
+#ifndef AppSDK_AppLibShared_h
+#define AppSDK_AppLibShared_h
+
+
+#endif
