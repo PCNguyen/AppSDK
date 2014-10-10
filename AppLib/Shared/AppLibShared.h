84bdee5fc7bbb46683518c39cdee4bf3454d6ac6
PC Nguyen <pnguyen@reputation.com>
2014-10-10 12:33:57 -0700

    adding useful macro for singleton

--- AppLib/Shared/AppLibShared.h
+++ AppLib/Shared/AppLibShared.h
@@ -9,5 +9,16 @@
 #ifndef AppSDK_AppLibShared_h
 #define AppSDK_AppLibShared_h
 
+/***************************************
+ * Macros
+ ***************************************/
+
+#define SHARE_INSTANCE_BLOCK(block) \
+	static dispatch_once_t onceToken; \
+	__strong static id _sharedObject = nil; \
+	dispatch_once(&onceToken, ^{ \
+		_sharedObject = block(); \
+	}); \
+	return _sharedObject; \
 
 #endif
