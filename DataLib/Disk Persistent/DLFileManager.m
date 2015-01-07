2700d479cce7aa2db8c7642487e4d96129511a1e
PC Nguyen <pnguyen@reputation.com>
2015-01-07 10:06:54 -0800

    fix clean up directory

--- DataLib/Disk Persistent/DLFileManager.m
+++ DataLib/Disk Persistent/DLFileManager.m
@@ -184,7 +184,7 @@ static NSString *const kFMLockName      = @"dl.filemanager.lock";
 		//--clean up the file on disk
 		[self.fileMetaData enumerateKeysAndObjectsUsingBlock:^(NSString *fileName, NSDate *expiredDate, BOOL *stop) {
 			if ([expiredDate timeIntervalSinceNow] <= 0) {
-				NSURL *directoryURL = [self urlForDocumentsDirectory];
+				NSURL *directoryURL = [self urlForCacheDirectory];
 				
 				if ([self.subDirectory length] > 0) {
 					directoryURL = [directoryURL URLByAppendingPathComponent:self.subDirectory];
